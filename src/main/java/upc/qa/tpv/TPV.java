b3524ce45f02e770e3f249d63aca98048ddc0fce
pier <pierlauros@TTC.CAT>
2019-03-05 19:36:09 +0100

    Demo venta automatica

--- src/main/java/upc/qa/tpv/TPV.java
+++ src/main/java/upc/qa/tpv/TPV.java
@@ -2,6 +2,7 @@ package upc.qa.tpv;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * 
@@ -9,8 +10,36 @@ import java.util.List;
  * 
  */
 public class TPV {
-	private List<Venta> ventas = new ArrayList<Venta>();
+	private static List<Venta> ventas = new ArrayList<Venta>();
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Venta venta = new Venta(new ArrayList<Codigo>());
+		
+		boolean enabled = true;
+		while(enabled) {
+			while(sc.hasNextLong()) {
+				Codigo code = new Codigo(sc.nextLong());
+				System.out.println("Adding code " + code.getCode());
+				venta.addCodigo(code);
+			}
+			while(sc.hasNextLine()) {
+				String line = sc.nextLine();
+				if(line.equals("stop") || line.length() == 1) {
+					enabled = false;
+					break;
+				}
+				ventas.add(venta);
+				System.out.println("Print ticket");
+				break;
+
+			}
+		}
+		
+		sc.close();
+	}
+	
 	/**
 	 *
 	 * Gets a list of the TPV's Ventas 
