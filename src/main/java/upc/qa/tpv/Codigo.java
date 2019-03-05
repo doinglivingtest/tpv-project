d2b7c12fb9d6577f6a93b9ff700db1d37ab42c89
Sara Flores Corral <sarafc1@ttc.cat>
2019-03-05 19:12:16 +0100

    Adding code for Bar Codes and Ventas

---
+++ src/main/java/upc/qa/tpv/Codigo.java
@@ -0,0 +1,14 @@
+package upc.qa.tpv;
+
+public class Codigo {
+    private long code;
+
+
+    public Codigo(long code) {
+        this.code = code;
+    }
+
+    public long getCode() {
+        return code;
+    }
+}
